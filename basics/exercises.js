3ae5f01d462e3eae446d9f92e92721edd2accc0a
Dave Carrillo <dqcrevive@gmail.com>
2020-01-20 10:10:54 -0800

    Adding basics folder

---
+++ basics/exercises.js
@@ -0,0 +1,75 @@
+// Exercises from "The Basics"
+/*
+// 1
+let firstName = 'Dave';
+let lastName = 'Carrillo';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+
+// 2
+let number = 4936;
+let ones = number % 10;
+ones //6
+
+number = (number - ones) / 10;
+//493
+
+let tens = number % 10;
+tens //3
+
+number = (number - tens) / 10;
+//49
+
+let hundreds = number % 10;
+hundreds //9
+
+let thousands = (number - hundreds) / 10;
+thousands //4
+
+*/
+
+// 3
+1. string
+2. boolean
+3. floating-point decimal number
+4. number
+5. undefined
+6. object
+
+// 4
+Lines 2 and 3
+
+// 5
+Because the + symbol concatenates, changing the number to a string.
+
+// 6
+console.log(Number('5') + 10);
+
+// 7
+`The value of 5 + 10 is ${5 + 10}`
+
+// 8
+Undefined
+
+// 9
+let names = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
+
+// 10
+let pets = {
+  asta: 'dog',
+  butterscotch: 'cat',
+  pudding: 'cat',
+  neptune: 'fish',
+  darwin: 'lizard',
+};
+
+// 11
+false
+
+// 12
+3
+
+// 13
+true
